844694071bac82b5307b5ca49d47c20e49639b50
Nyralim Sidhe <79785064+eliapetrosino@users.noreply.github.com>
2022-01-03 21:54:24 +0100

    Update Mediator.cs

--- Contracts/Mediator.cs
+++ Contracts/Mediator.cs
@@ -2,7 +2,9 @@ using System;
 namespace Mediator_Pattern
 {
    public interface IMediator {
+      void AcceptLandingRequest(Airplane colleague);
       void EnqueueLandingRequest(Airplane colleague);
+      void RejectLandingRequest(Airplane colleague);
    }
 
 }
